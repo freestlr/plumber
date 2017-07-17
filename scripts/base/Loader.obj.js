138297e6bfe79e6c8048a75b919f1c31b1121712
freestlr <freestlr@gmail.com>
2017-07-17 19:20:46 +0300

    Loader.obj understand negative vertex indices; increase parse performance

--- scripts/base/Loader.obj.js
+++ scripts/base/Loader.obj.js
@@ -4,7 +4,7 @@ Loader.extend('obj', {
 	saveMetadata: false,
 	computeBoundingBox: false,
 
-	prepare: function(data) {
+	prepare: function(text) {
 		var unit = this
 
 		var meta = {
@@ -28,8 +28,13 @@ Loader.extend('obj', {
 
 
 		function addFaceVertex(index) {
-			if(index < 0) index = meta.vertices.length + index +1
-			if(!meta.vertices[index]) console.warn('Loader.obj: undefined vertex '+ index)
+			if(index < 0) {
+				index = meta.vertices.length + index +1
+			}
+
+			if(!meta.vertices[index]) {
+				console.warn('Loader.obj: undefined vertex '+ index)
+			}
 
 			if(index < vertexOffset) {
 				return geometry.vertices.push(meta.vertices[index]) -1
@@ -48,6 +53,25 @@ Loader.extend('obj', {
 			meta.meshes.push(mesh)
 		}
 
+		function addFace(a, b, c, index, uv, normal, vertex, hasUV, hasNR, line) {
+			v1.subVectors(vertex[c], vertex[b])
+			v2.subVectors(vertex[a], vertex[b])
+			v1.cross(v2)
+
+			if(!v1.x && !v1.y && !v1.z) {
+				console.warn('[Loader.obj] collapsed face "'+ unit.url +'":', line, [a, b, c])
+				return
+			}
+			v1.normalize()
+
+			var face = new THREE.Face3(index[a], index[b], index[c])
+			face.normal.copy(v1)
+			if(hasNR) face.vertexNormals.push(normal[a], normal[b], normal[c])
+
+			geometry.faceVertexUvs[0].push(hasUV ? [uv[a], uv[b], uv[c]] : uv0)
+			geometry.faces.push(face)
+		}
+
 		function commitMesh() {
 			if(!geometry || !geometry.vertices.length) return
 
@@ -64,10 +88,15 @@ Loader.extend('obj', {
 			addMesh()
 		}
 
-		var lines = data.split('\n')
+		var length = text.length
+		var lineNo = -1
+		var index = 0
+		while(index !== -1) {
+			var nextIndex = text.indexOf('\n', index +1)
+			var line = text.substr(index, nextIndex - index).replace(/#.*/, '').trim()
 
-		for(var i = 0; i < lines.length; i++) {
-			var line = lines[i].replace(/#.*/, '').trim()
+			lineNo++
+			index = nextIndex
 			if(!line) continue
 
 			var parts = line.split(/\s+/)
@@ -78,23 +107,20 @@ Loader.extend('obj', {
 					// ignore
 				break
 
+				case 'o': // object
+					// ignore
+				break
+
+				case 's': // shading
+					// ignore
+				break
+
 				case 'usemtl': // material
 					mesh.material.name = data.join(' ')
 				break
 
 				case 'g': // group
 					mesh.name = data.join(' ')
-				break
-
-				case 'o': // object
-					//    ignore
-					// object = new THREE.Object3D
-					// object.name = data.join(' ')
-					// root.add(object)
-				break
-
-				case 's': // shading
-					// ignore
 				break
 
 				case 'v': // vertex
@@ -123,53 +149,49 @@ Loader.extend('obj', {
 				break
 
 				case 'f': // face: vertex/uv/normal
-					if(data.length === 4) { // split face4 into two face3: 1,2,4 and 2,3,4
-						lines.splice(i +1, 0, ['f', data[1], data[2], data[3]].join(' '))
-						data.splice(2, 1)
-					}
+					var vertices = data.length
 
-					var vgroup = []
+					var hasNR = true
+					,   hasUV = true
+
+					var igroup = []
 					,   ugroup = []
 					,   ngroup = []
-					for(var j = 0; j < 3; j++) {
+					,   vgroup = []
+					for(var j = 0; j < vertices; j++) {
 						var fd = data[j].split('/')
 
-						vgroup.push(addFaceVertex(parseInt(fd[0]) -1))
-						ugroup.push(     meta.uvs[parseInt(fd[1]) -1])
-						ngroup.push( meta.normals[parseInt(fd[2]) -1])
+						var ig = addFaceVertex(parseInt(fd[0]) -1)
+						,   ug =      meta.uvs[parseInt(fd[1]) -1]
+						,   ng =  meta.normals[parseInt(fd[2]) -1]
+
+						igroup.push(ig)
+						ugroup.push(ug)
+						ngroup.push(ng)
+						vgroup.push(geometry.vertices[ig])
+
+						hasUV &= !!ug
+						hasNR &= !!ng
 					}
 
-					var vA = geometry.vertices[vgroup[0]]
-					,   vB = geometry.vertices[vgroup[1]]
-					,   vC = geometry.vertices[vgroup[2]]
+					if(vertices === 4) {
+						addFace(0, 1, 3, igroup, ugroup, ngroup, vgroup, hasUV, hasNR, lineNo)
+						addFace(1, 2, 3, igroup, ugroup, ngroup, vgroup, hasUV, hasNR, lineNo)
 
-					v1.subVectors(vC, vB)
-					v2.subVectors(vA, vB)
-					v1.cross(v2)
+					} else if(vertices === 3) {
+						addFace(0, 1, 2, igroup, ugroup, ngroup, vgroup, hasUV, hasNR, lineNo)
 
-					if(!v1.x && !v1.y && !v1.z) {
-						console.warn('[Loader.obj] collapsed face "'+ unit.url +'":', data.join(' '))
+					} else {
+						console.warn('[Loader.obj] unknown face with', vertices, 'vertices')
 						continue
 					}
-					v1.normalize()
-
-
-					var hasNR = ngroup[0] && ngroup[1] && ngroup[2]
-					,   hasUV = ugroup[0] && ugroup[1] && ugroup[2]
-
-					var face = new THREE.Face3(vgroup[0], vgroup[1], vgroup[2])
-
-					face.normal.copy(v1)
-					if(hasNR) face.vertexNormals = ngroup
-
-					geometry.faceVertexUvs[0].push(hasUV ? ugroup : uv0)
-					geometry.faces.push(face)
 
 					createMesh = true
 				break
 
 				default:
 					console.log('Loader.obj: unhandled "'+ line +'"')
+				break
 			}
 		}
 
