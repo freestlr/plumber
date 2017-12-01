34d3a50a1dd80c84d07dfc0a810b6e32bb2bacb1
freestlr <freestlr@gmail.com>
2017-12-01 17:33:40 +0300

    update Atlas.js: common version

--- scripts/base/Atlas.js
+++ scripts/base/Atlas.js
@@ -1,5 +1,6 @@
 Atlas = {
 	svg: null,
+	list: null,
 	items: [],
 
 	setSource: function(svg) {
@@ -8,6 +9,23 @@ Atlas = {
 		Atlas.svg = svg.documentElement || svg
         Atlas.svg.removeAttribute('width')
         Atlas.svg.removeAttribute('height')
+
+		Atlas.list = {}
+
+		var elements = Atlas.svg.getElementsByTagName('*')
+		for(var i = 0; i < elements.length; i++) {
+			var element = elements[i]
+			,   nodeName = element.nodeName.toLowerCase()
+
+			if(nodeName === 'metadata') {
+				if(element.parentNode) element.parentNode.removeChild(element)
+
+			} else if(element.id) {
+				element.removeAttribute('visibility')
+				Atlas.list[element.id] = element
+			}
+		}
+
 		Atlas.update()
 	},
 
@@ -16,11 +34,7 @@ Atlas = {
 	},
 
 	set: function(element, id, name) {
-		var item = f.apick(Atlas.items, 'element', element)
-		if(!item) {
-			item = { element: element }
-			Atlas.items.push(item)
-		}
+		var item = Atlas.pickItem(element)
 		item.id = id
 		item.name = name
 
@@ -33,40 +47,49 @@ Atlas = {
 		for(var i = Atlas.items.length -1; i >= 0; i--) {
 			var item = Atlas.items[i]
 
-			if(item.element === element) {
-				dom.remove(item.icon)
+			if(!element || item.element === element) {
+				item.element.removeChild(item.icon)
 				Atlas.items.splice(i, 1)
 				return
 			}
 		}
 	},
 
+	pickItem: function(element) {
+		for(var i = 0; i < Atlas.items.length; i++) {
+			var item = Atlas.items[i]
+			if(item.element === element) return item
+		}
+		item = { element: element }
+		Atlas.items.push(item)
+		return item
+	},
+
 	updateItem: function(item) {
-		var icon = Atlas.get(item.id)
+		var icon = Atlas.clone(item.id)
 
 		if(icon) {
 			if(item.name) icon.className.baseVal += ' '+ item.name
 
 		} else {
-			icon = dom.div(item.name)
-			dom.text(icon, item.id)
+			icon = document.createElement('div')
+			icon.className = item.id +' '+ item.name
+			if(Atlas.svg) icon.textContent = item.id
 		}
 
 		if(item.icon) {
-			dom.insert(item.element, icon, item.icon)
-			dom.remove(item.icon)
+			item.element.insertBefore(icon, item.icon)
+			item.element.removeChild(item.icon)
 
 		} else {
-			dom.append(item.element, icon)
+			item.element.appendChild(icon)
 		}
 
 		item.icon = icon
 	},
 
-	get: function(id) {
-		if(!Atlas.svg) return null
-
-		var icon = Atlas.svg.getElementById(id)
+	clone: function(id) {
+		var icon = Atlas.list && Atlas.list[id]
 		if(!icon) return null
 
 		var svg = Atlas.svg.cloneNode(false)
